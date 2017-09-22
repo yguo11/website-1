fb8cdcdbde699e158f3d9c254524f2ca56cad6d6
yguo11 <yuanjing56@gmail.com>
2017-09-22 14:11:20 -0700

    website-1

---
+++ main.js
@@ -0,0 +1,14 @@
+// When the user clicks the button (#showModal)
+  // Fade in the modal (.modal-background)
+
+
+
+// When the user clicks the close button (#close)
+  // Fade out the modla (.modal-background)
+
+
+
+
+$('.sfbc-mobile').on('mouseenter', function(){
+  $('.sfbc-mobile').fadeIn('300');
+})
